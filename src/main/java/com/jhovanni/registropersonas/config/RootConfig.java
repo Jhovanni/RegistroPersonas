ab3cff4900ecd1c1e2ac9ab643e48bbf88efb7c2
Jhovanni <ejhova@gmail.com>
2015-02-11 16:33:51 -0600

    Configurado Hibernate 4

--- src/main/java/com/jhovanni/registropersonas/config/RootConfig.java
+++ src/main/java/com/jhovanni/registropersonas/config/RootConfig.java
@@ -1,11 +1,55 @@
 package com.jhovanni.registropersonas.config;
 
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  *
  * @author Jhovanni
  */
 @Configuration
+@ComponentScan(basePackages = "com.jhovanni.registropersonas.hibernate")
+@EnableTransactionManagement
 class RootConfig {
+
+    @Bean
+    public BasicDataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        dataSource.setUrl("jdbc:oracle:thin:@localhost:1521");
+        dataSource.setUsername("registro_personas");
+        dataSource.setPassword("registro_personas");
+        return dataSource;
+    }
+
+    @Bean
+    @Autowired
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.Oracle10gDialect");
+
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setPackagesToScan("com.jhovanni.registropersonas.entidad");
+        sessionFactory.setHibernateProperties(properties);
+        return sessionFactory;
+    }
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory);
+        return transactionManager;
+
+    }
 }
