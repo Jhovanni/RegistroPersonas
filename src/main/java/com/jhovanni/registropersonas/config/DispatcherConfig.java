2a094497c396ed595e8d168af7c5ca2d2d8db43c
Jhovanni <ejhova@gmail.com>
2015-02-12 11:30:22 -0600

    Añadidas dependencias para subir archivos

--- src/main/java/com/jhovanni/registropersonas/config/DispatcherConfig.java
+++ src/main/java/com/jhovanni/registropersonas/config/DispatcherConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -32,5 +33,10 @@ public class DispatcherConfig extends WebMvcConfigurerAdapter {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
         messageSource.setBasename("/WEB-INF/locale/bundle");
         return messageSource;
+    }
+    @Bean
+    public CommonsMultipartResolver multipartResolver(){
+        CommonsMultipartResolver multipartResolver=new CommonsMultipartResolver();
+        return multipartResolver;
     }
 }
