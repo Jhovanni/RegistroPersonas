399928935e96fb463247802c2908e9bbd4d435dc
Jhovanni <ejhova@gmail.com>
2015-06-07 19:53:19 -0500

    Revert "Revert "Integración de Mockito, test para métodos sin restricción de acceso incluídos""
    This reverts commit b4780aa960dd121b21a73389e4d8409de6adbba6.

--- src/test/java/com/jhovanni/registropersonas/config/TestSessionFactory.java
+++ src/test/java/com/jhovanni/registropersonas/config/TestSessionFactory.java
@@ -1,12 +1,8 @@
-package com.jhovanni.registropersonas.hibernate;
+package com.jhovanni.registropersonas.config;
 
-import com.jhovanni.registropersonas.config.RootConfig;
-import com.jhovanni.registropersonas.entidad.Usuario;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -18,14 +14,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @author Jhovanni
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = RootConfig.class)
+@ContextConfiguration(classes = {RootConfig.class})
 public class TestSessionFactory {
-
-    @Autowired
-    private Servicio servicio;
     @Autowired
     private SessionFactory sessionFactory;
-
     /**
      * Test conexión a base de datos Comprueba que la base de datos reponda a la
      * solicitud de conexión correctamente
