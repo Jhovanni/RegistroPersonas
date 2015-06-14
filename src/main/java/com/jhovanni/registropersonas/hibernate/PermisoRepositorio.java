5371610796c86a2ba4d2e638666bdd3e64e97094
Jhovanni <ejhova@gmail.com>
2015-06-14 15:53:27 -0500

    Loggin a todos los métodos de repositorios Task #3 - Agregar loggin a todos los repositorios

--- src/main/java/com/jhovanni/registropersonas/hibernate/PermisoRepositorio.java
+++ src/main/java/com/jhovanni/registropersonas/hibernate/PermisoRepositorio.java
@@ -3,6 +3,8 @@ package com.jhovanni.registropersonas.hibernate;
 import com.jhovanni.registropersonas.entidad.Permiso;
 import java.io.Serializable;
 import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,47 +16,57 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class PermisoRepositorio implements Repositorio<Permiso> {
+    Logger log = LogManager.getLogger();
 
     @Autowired
     private SessionFactory sessionFactory;
 
     @Override
     public List<Permiso> get() {
+        log.entry();
         Session session = sessionFactory.getCurrentSession();
-        return session.createQuery("from Permiso").list();
+        return log.exit(session.createQuery("from Permiso").list());
     }
 
     @Override
     public List<Permiso> buscar(String busqueda) {
+        log.entry(busqueda);
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public Permiso get(Serializable id) {
+        log.entry(id);
         Session session = sessionFactory.getCurrentSession();
-        return (Permiso) session.get(Permiso.class, id);
+        return log.exit((Permiso) session.get(Permiso.class, id));
     }
 
     @Override
     public Serializable crear(Permiso permiso) {
+        log.entry(permiso);
         Session session = sessionFactory.getCurrentSession();
-        return session.save(permiso);
+        return log.exit(session.save(permiso));
     }
 
     @Override
     public void guardar(Permiso permiso) {
+        log.entry(permiso);
         Session session = sessionFactory.getCurrentSession();
         session.update(permiso);
+        log.exit();
     }
 
     @Override
     public void borrar(Permiso permiso) {
+        log.entry(permiso);
         Session session = sessionFactory.getCurrentSession();
         session.delete(permiso);
+        log.exit();
     }
 
     @Override
     public Permiso get(String cadena) {
+        log.entry(cadena);
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
